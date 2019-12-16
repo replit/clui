950ecf3b4b6127c016e424c433e54e38dd7c8732
Moudy <moudy.elkammash@gmail.com>
2019-12-16 12:30:11 +0000

    build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [
+    typescript(),
+    babel({
+      exclude: ["node_modules/**"]
+    })
+  ]
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// import Session from './Session
+
+export { default as Session } from "./Session";
